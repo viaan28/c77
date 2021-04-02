1885ff6425b03582c27466acb20c348322e72c96
hacker <76834154+viaan28@users.noreply.github.com>
2021-04-02 13:53:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("MY FIRST CONSOLE PROJECT");
+var first_name="Viaan";
+var second_name=" Kataria";
+var full_name=first_name.concat(second_name);
+console.log (full_name);
+var first_number=73;
+var second_number=69;
+var result_number=first_number+second_number;
+console.log(result_number);
